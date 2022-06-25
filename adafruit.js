81519b8ec316b9627757160b93cb5ff082eddd71
Logan <ultimate360solutions@gmail.com>
2022-06-25 14:09:13 -0500

    finished discord interaction logic

--- adafruit.js
+++ adafruit.js
@@ -3,18 +3,20 @@ const jsdom = require('jsdom');
 const chalk = require('chalk');
 const fs = require('fs');
 const { JSDOM } = jsdom;
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { MessageEmbed, Client, Intents, ShardClientUtil } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], shards: 'auto' });
+const clientShardHelper = new ShardClientUtil(client);
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+let configuredGuild; // the discord guild to send the stock status to (gets initialized in the ready event)
 
 // connect to discord
 client.login(config.discordBotToken);
 
+
 // query the Adafruit website for the stock stats of all models of the Raspberry Pi 4 Model B
-async function checkStockStatus() {
+function checkStockStatus() {
   axios.get('https://www.adafruit.com/product/4295')
     .then(function (response) {
-
       // on success, parse the HTML response into a DOM object
       const html = response.data;
       const dom = new JSDOM(html);
@@ -25,24 +27,23 @@ async function checkStockStatus() {
       // gather the stock status of each model (represented as a boolean for being in-stock or not)
       const oneGigModelInStock = stockList[0].textContent.includes('In stock');
       const twoGigModelInStock = stockList[1].textContent.includes('In stock');
-      const fourGigModelInStock = stockList[2].textContent.includes('In stock');
+      const fourGigModelInStock = true; //stockList[2].textContent.includes('In stock');
       const eightGigModelInStock = stockList[3].textContent.includes('In stock');
-      console.log(oneGigModelInStock, twoGigModelInStock, fourGigModelInStock, eightGigModelInStock);
 
       // send the stock status to discord if any of the models are in stock
       if (oneGigModelInStock || twoGigModelInStock || fourGigModelInStock || eightGigModelInStock) {
+        // report what is in stock
+        console.log(chalk.green(`These models are in STOCK: ${oneGigModelInStock ? '1GB' : ''} ${twoGigModelInStock ? '2GB' : ''} ${fourGigModelInStock ? '4GB' : ''} ${eightGigModelInStock ? '8GB' : ''}`));
         sendToDiscord(oneGigModelInStock, twoGigModelInStock, fourGigModelInStock, eightGigModelInStock);
       }
     })
     .catch(function (error) {
-      console.log('Error occurred during request to Adafruit:', error);
+      console.error(chalk.red('An error occurred during the status refresh:\n'), error);
     });
 }
 
 // schedule the stock status update to be called at the specified interval
-setInterval(async () => {
-  await checkStockStatus();
-}, config.updateIntervalSeconds * 1000);
+setInterval(() => { checkStockStatus(); }, config.updateIntervalSeconds * 1000);
 
 
 //**********************************
@@ -50,9 +51,16 @@ setInterval(async () => {
 //**********************************
 
 client.on('ready', () => {
-  console.log(chalk.greenBright(`Logged in as ${client.user.tag}!`));
+  console.log(chalk.greenBright(`Logged in as ${client.user.tag} in ${client.guilds.cache.size} servers while using ${clientShardHelper.count} shard(s)!`));
   // set the bot's presence
-  client.user.setActivity('for Pis\'s!', { type: 'WATCHING' });
+  client.user.setActivity('for Pis!', { type: 'WATCHING' });
+  // get the discord guild to send the stock status to
+  try {
+    configuredGuild = client.guilds.cache.get(config.discordServerID);
+  }
+  catch (err) {
+    console.error(chalk.red(`Error looking up guild with provided ID ${config.discordServerID}\n:`), err);
+  }
   // run a stock status check on startup (will run on configured interval after this)
   checkStockStatus();
 });
@@ -60,66 +68,94 @@ client.on('ready', () => {
 // Logs additions of new servers
 client.on('guildCreate', guild => {
   console.log(chalk.green('NEW SERVER: ' + chalk.cyan(guild.name)));
-  setupServer(guild);
+  //setupServer(guild);
 });
+
 
 
 //**********************************
 //*       Utility Functions        *
 //**********************************
 
-function setupServer() {
-// first, create the roles roles for the server
-  createRoles();
-  // create the notification channel
-  createNotificationChannel();
-}
+// function setupServer() {
+//   // first, create the roles roles for the server
+//   createRoles();
+//   // create the notification channel
+//   createNotificationChannel();
+// }
 
 
-function sendToDiscord(){
+function sendToDiscord(oneGigModelInStock, twoGigModelInStock, fourGigModelInStock, eightGigModelInStock) {
+
   let mentionRolesMessage = ''; // will be populated with the roles to mention based on status of each model
+  console.log(chalk.greenBright('Sending stock status to Discord...'));
+
+  // grab the roles cache from the configured guild
+  const rolesCache = configuredGuild.roles.cache;
+
+  // grab the channels cache from the configured guild
+  const channelsCache = configuredGuild.channels.cache;
 
   // create the template embed to send to discord
   const embed = new MessageEmbed()
     .setTitle('Adafruit Raspberry Pi 4 IN STOCK!')
     .setDescription('The following models are in stock:\n')
     .setColor('#00ff00')
-    .setThumbnail('https://cdn-shop.adafruit.com/970x728/4564-00.jpg')
+    .setThumbnail('https://cdn-shop.adafruit.com/970x728/4292-06.jpg')
     .setFooter({
-      text: 'Check Me Out on GitHub!',
-      iconURL: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-      url: 'https://github.com/EthyMoney/Adafruit-Pi4-Stock-Bot'
+      text: 'github.com/EthyMoney/Adafruit-Pi4-Stock-Bot',
+      iconURL: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
     });
 
   // populate stock fields for all in-stock models where notification is enabled in the config
   if (oneGigModelInStock && config.watch1GigModel) {
     embed.addField('1GB Model', '[BUY IT!](https://www.adafruit.com/product/4295)', true);
-    const oneGigRoleID = client.guilds.cache.get(config.discordServerID).roles.cache.find(role => role.name === 'Pi4 1GB').id;
-    mentionRolesMessage += (oneGigRoleID) ? ` <@!${oneGigRoleID}}> ` : console.log('No 1GB role found!');
+    const oneGigRole = rolesCache.find(role => role.name === 'Pi4 1GB');
+    mentionRolesMessage += (oneGigRole) ? ` <@!${oneGigRole.id}}> ` : console.error(chalk.red('No 1GB role found!'));
   }
   if (twoGigModelInStock && config.watch2GigModel) {
     embed.addField('2GB Model', '[BUY IT!](https://www.adafruit.com/product/4292)', true);
-    const twoGigRoleID = client.guilds.cache.get(config.discordServerID).roles.cache.find(role => role.name === 'Pi4 1GB').id;
-    mentionRolesMessage += (twoGigRoleID) ? ` <@!${twoGigRoleID}}> ` : console.log('No 2GB role found!');
+    const twoGigRole = rolesCache.find(role => role.name === 'Pi4 2GB');
+    console.log('no', twoGigRole);
+    mentionRolesMessage += (twoGigRole) ? ` <@!${twoGigRole.id}}> ` : console.error(chalk.red('No 2GB role found!'));
   }
   if (fourGigModelInStock && config.watch4GigModel) {
     embed.addField('4GB Model', '[BUY IT!](https://www.adafruit.com/product/4296)', true);
-    const fourGigRoleID = client.guilds.cache.get(config.discordServerID).roles.cache.find(role => role.name === 'Pi4 1GB').id;
-    mentionRolesMessage += (fourGigRoleID) ? ` <@!${fourGigRoleID}}> ` : console.log('No 4GB role found!');
+    const fourGigRole = rolesCache.find(role => role.name === 'Pi4 4GB');
+    mentionRolesMessage += (fourGigRole) ? ` <@!${fourGigRole}}> ` : console.error(chalk.red('No 4GB role found!'));
   }
   if (eightGigModelInStock && config.watch8GigModel) {
     embed.addField('8GB Model', '[BUY IT!](https://www.adafruit.com/product/4564)', true);
-    const eightGigRoleID = client.guilds.cache.get(config.discordServerID).roles.cache.find(role => role.name === 'Pi4 1GB').id;
-    mentionRolesMessage += (eightGigRoleID) ? ` <@!${eightGigRoleID}}> ` : console.log('No 8GB role found!');
+    const eightGigRole = rolesCache.find(role => role.name === 'Pi4 8GB');
+    console.log('no', eightGigRole);
+    mentionRolesMessage += (eightGigRole) ? ` <@!${eightGigRole}}> ` : console.error(chalk.red('No 8GB role found!'));
   }
 
-  // lookup the configured discord channel ID by name and send the embed out to the channel
-  const channel = client.channels.cache.find(channel => channel.name === config.discordChannelName);
-  channel.send({ embeds: [embed] })
-    .catch(function (reject) {
-      console.log(chalk.red(`Error sending message to ${channel.guild.name} with promise rejection: ${reject}`));
-    });
+  // lookup the configured discord TEXT channel by name and send the embed out to the channel
+  const channel = channelsCache.find(channel => channel.name === config.discordChannelName.toString() && channel.type == 'GUILD_TEXT');
 
-  // also mention all the relevant users that have the applicable model roles
-  channel.send(mentionRolesMessage.trim());
+  // if the channel was found, send the embed and mention messages
+  if (channel) {
+    channel.send({ embeds: [embed] })
+      .then(() => {
+        console.log(chalk.greenBright('Successfully sent notification EMBED to Discord!'));
+      })
+      .catch(function (reject) {
+        console.error(chalk.red(`Error sending EMBED message to server ${chalk.cyan(configuredGuild.name)} with promise rejection: ${reject}`));
+      });
+
+    // also mention all the relevant users that have the applicable model roles (if the roles could be found in the server)
+    if (mentionRolesMessage) {
+      channel.send(mentionRolesMessage.trim())
+        .then(() => {
+          console.log(chalk.greenBright('Successfully sent MENTION message to Discord!'));
+        })
+        .catch(function (reject) {
+          console.error(chalk.red(`Error sending MENTION message to server ${chalk.cyan(configuredGuild.name)} with promise rejection: ${reject}`));
+        });
+    }
+  }
+  else {
+    console.error(chalk.red('No text channel found in server with name: ' + chalk.cyan('"' + config.discordChannelName + '"')), chalk.yellow('check config file! (verify CaSe and spelling)'));
+  }
 }
